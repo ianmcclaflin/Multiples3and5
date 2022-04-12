e1161f743a53d0691f78f584686ea733776d1008
Ian McClaflin <ian.mcclaflin@outlook.com>
2022-04-11 20:34:45 -0500

    Initial Commit

--- Multiples3and5/Program.cs
+++ Multiples3and5/Program.cs
@@ -10,6 +10,44 @@ namespace Multiples3and5
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 1000; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                } 
+
+                
+            }
+
+
+            List<int> numbers = new List<int>();
+            for (int i = 3; i < 1000; i+= 3)
+            {
+                numbers.Add(i);
+            }
+            for (int j = 5 ; j < 1000; j+= 5)
+            {
+                numbers.Add(j);
+            }
+            
+            int sum1 = numbers.Sum();
+            Console.WriteLine("This is the sum of all 3 and 5 multiples from 0-1000");
+            Console.WriteLine(sum1);
+            
+                
+
         }
+
+
+
+
+
+        
     }
 }
